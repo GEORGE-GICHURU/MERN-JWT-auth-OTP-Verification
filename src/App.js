0000840f8576f8b5e4af614adc474c94abbc401e
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-01-24 11:18:30 +0300

    import the components and created react routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const App = () => {
+/** import all the components */
+import  Username  from './components/Username';
+import  Reset  from './components/Reset';
+import  Register  from './components/Register';
+import  Recovery  from './components/Recovery';
+import  Profile  from './components/Profile';
+import  Password  from './components/Password';
+import  PageNotFound  from './components/PageNotFound';
+
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <Username></Username>
+  },
+
+  {
+    path: "/register",
+    element: <Register></Register>
+  },
+
+  {
+    path: "/password",
+    element: <Password></Password>
+  },
+
+  {
+    path: "/reset",
+    element: <Reset></Reset>
+  },
+
+  {
+    path: "/*",
+    element: <PageNotFound></PageNotFound>
+  },
+
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>
+  },
+
+  {
+    path: "/profile",
+    element: <Profile></Profile>
+  },
+])
+
+export default function App () {
   return (
-    <div>App</div>
+    <main>
+      <RouterProvider router={router}>
+
+      </RouterProvider>
+    </main>
+
   )
 }
 
-export default App
